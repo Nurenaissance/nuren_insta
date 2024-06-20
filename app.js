90b0f2273a5a0d8b8656da0b32fa4f2a8f494f6a
Nurenaissance <154663519+Nurenaissance@users.noreply.github.com>
2024-06-20 15:56:30 +0530

    20.6.24

--- app.js
+++ app.js
@@ -12,21 +12,23 @@ let pageID ;
 let userIGSID;
 
 const allowedOrigins =['*']
-app.use((req, res, next) =>{
-    const origin = req.headers.origin;
-    if(allowedOrigins.includes(origin)){
-      res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.header(
-      'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET, POST, PUT, DELETE, OPTIONS, PATCH'
-    );
-    next();
-  });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-CSRF-Token, X-Api-Version'
+  );
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTIONS, PATCH'
+  );
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
   
 
 const inputMap = new Map();
